eaccbf9777f5ba4d85f40463b030c3c0d8661116
amod1811 <ammch101@gmail.com>
2023-01-02 17:48:55 +0530

    Added main program of MoodAnalyser

---
+++ MoodAnalyserMain.java
@@ -0,0 +1,20 @@
+package bl.com.moodanalyser;
+
+/**
+ * @author Amod
+ */
+public class MoodAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser program");
+
+        //Crate object
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+        // pass Happy message
+        System.out.println(moodAnalyser.analysemood("I am in Happy Mood"));
+
+        //pass sad message
+
+        System.out.println(moodAnalyser.analysemood("I am in  Sad Mood"));
+    }
+}
