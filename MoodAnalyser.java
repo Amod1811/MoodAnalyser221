616e7c1aba2e40495f88b5ee9780bac43f1e932f
amod1811 <ammch101@gmail.com>
2023-01-02 17:54:28 +0530

    Added program check message mood wiil be sad or Happy

---
+++ MoodAnalyser.java
@@ -0,0 +1,21 @@
+package bl.com.moodanalyser;
+
+public class MoodAnalyser {
+    static String message;
+
+    /**
+     *
+     * @param message- will pass to the method
+     * @return - it will "HAPPY" or "SAD"
+     */
+    public String analysemood(String message) {
+        if (message.equalsIgnoreCase("I am in sad Mood"))
+            return "SAD";
+        else if (message.equalsIgnoreCase("I am in happy Mood"))
+            return "HAPPY";
+        else
+            return "SAD";
+
+
+    }
+}
