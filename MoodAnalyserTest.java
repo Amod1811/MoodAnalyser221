0b3cd710f03de7be9d7a619d4964946c306438d2
amod1811 <ammch101@gmail.com>
2023-01-07 23:19:52 +0530

    Added: Program to pass the message to use method

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -6,23 +6,6 @@ import org.testng.annotations.Test;
 public class MoodAnalyserTest {
     /**
      * create  MoodAnalyser class object
-     * check Message "I am in sad mood" or not
-     * if message is "I am in sad mood"
-     * @return - it will return SAD
-     */
-
-    @Test
-    public void givenMessageWhenSadShouldReturnSad(){
-
-        //create object
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String expectedValue = "SAD";
-        String mood = moodAnalyser.analysemood("I am in sad mood");
-        Assert.assertEquals(expectedValue,mood);
-    }
-
-    /**
-     * create  MoodAnalyser class object
      * check message is "I am in Happy mood" or not
      * if message  is "I am in Happy mood"
      * @return - it will return HAPPY
@@ -35,4 +18,15 @@ public class MoodAnalyserTest {
         String mood = moodAnalyser.analysemood("I am in Happy mood");
         Assert.assertEquals(expectedValue,mood);
     }
+    @Test
+    public void givenMessageWhenSadShouldReturnSad(){
+
+        //create object
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String expectedValue = "SAD";
+        String mood = moodAnalyser.analysemood("I am in sad Mood");
+        Assert.assertEquals(expectedValue,mood);
+    }
+
+
 }
